ef5a4ea64c2f7279bb5ce6bf38fdab8eb547cf9f
Michal Fojtik <mfojtik@redhat.com>
2014-12-11 11:45:32 +0100

    More comments

--- hack/build.sh
+++ hack/build.sh
@@ -1,24 +1,44 @@
 #!/bin/bash -e
+#
+# This is an example of the Origin custom builder that builds a minimal Docker
+# image with static Go binary.
+# This requires the project you are going to build has 'main.go' in the root
+# repository folder and it is hosted on Github.
 
-GO_SOURCE="github.com/mfojtik/go-sample-app"
-GO_BINARY="go-sample-app"
-IMAGE_NAME="openshift/origin-go-sample"
-IMAGE_TAG="prod"
-TARGET_IMAGE="172.121.17.3:5001/${IMAGE_NAME}:${IMAGE_TAG}"
+# Convert SOURCE_URI which is a full GIT url into a Go package name format
+PACKAGE_NAME=$(echo -n $SOURCE_URI | sed -e 's/^git:\/\///' | sed -e 's/\.git$//')
 
-# Build a static Go binary and create minimal Docker image from it
+# FIXME: This will work only for github.com/name/project and it assume you have
+#        'main.go' in the repository root
+BINARY_NAME=$(echo -n $PACKAGE_NAME | cut -d '/' -f 3)
+
+# The OUTPUT_IMAGE and OUTPUT_REGISTRY are sent from the Origin and they are
+# part of the 'Build' (you can also query them from $BUILD variable)
+DOCKER_REGISTRY="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+
+# The 'EXPOSE_PORT' can be defined as an environment variable for the custom
+# build (see: Build.parameters.strategy.customStrategy.env[])
+EXPOSE_PORT=${EXPOSE_PORT:-"8080"}
+
+# This will download the Go package, compile it and build a static binary.
 CGO_ENABLED=0 go get -a -ldflags '-s' ${GO_SOURCE}
-(cd /gopath/bin && tar cv ${GO_BINARY}) | docker import - ${IMAGE_NAME}-scratch
 
-# The final image Dockerfile
+# Convert the static Go binary into minimal Docker image
+(cd /gopath/bin && tar cv ${BINARY_NAME}) | docker import - ${OUTPUT_IMAGE}-scratch
+
+# Augument the minimal Docker image with EXPOSE and with CMD
 cat > Dockerfile <<- EOF
-FROM ${IMAGE_NAME}-scratch
-EXPOSE 8080
-CMD ["${GO_BINARY}"]
+FROM ${OUTPUT_IMAGE}-scratch
+EXPOSE ${EXPOSE_PORT}
+CMD ["${BINARY_NAME}"]
 EOF
 
-# Assemble the final scratch image, remove the temporary scratch image and push
-# to registry.
-docker build -t ${TARGET_IMAGE} .
-docker rmi ${IMAGE_NAME}-scratch
+# Build the final minimal Docker image with Go static binary
+docker build -t ${OUTPUT_IMAGE} .
+docker tag ${OUTPUT_IMAGE} ${DOCKER_REGISTRY}
+
+# Remove the temporary Docker image
+docker rmi ${OUTPUT_IMAGE}-scratch
+
+# Push the image to Docker registry
 docker push ${TARGET_IMAGE}
